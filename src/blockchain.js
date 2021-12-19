a9396cf64ca0d5d6c3f735d84d2e3410b4abe81d
George Campbell <geocolumbus@gmail.com>
2021-12-19 09:02:30 -0500

    Update README

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,14 +1,45 @@
 /*
+This code is based on three lessons on Simply Explained for creating a blockchain coin in javascript:
+
 Create a block chain - https://www.youtube.com/watch?v=zVqczFZr124
 Proof of work implementation - https://www.youtube.com/watch?v=HneatE69814
 Implement transactions - https://www.youtube.com/watch?v=fRV6cGXVQ4I
 
- */
+This code has all the basic elements required for a crypto currency, but it is not secure enough
+to be used as-is.
 
-console.clear()
+A block is a javascript object:
+
+timestamp    - unix time, ie 1639918141
+transactions - an array of transactions
+               - to address
+               - from address
+               - amount
+nonce        - an integer that is incremented until the hash meets the "proof of work" requirement.
+previousHash - the value of the hash of the previous block in the chain
+hash         - the SHA256 hash of the timestamp, nonce, previous hash and transactions
+
+A chain is an array of blocks
+
+The proof of work requires that the hash begin with a number of zeros determined by the difficulty. The
+block's payload is modified by incrementing a nonce until the proof of work is required.
+
+difficulty  time
+----------  ---------
+1            0.2 msec
+2            1.7 msec
+3           26.5 msec
+4          336.8 msec
+5            5.1 sec
+6           87.0 sec
+
+The time it takes to calculate the hash increases by a factor of 16 for each difficulty level. This means
+generating a hash that begins with 10 zeros would take my m1 macbook air 87*16^4 => 65 days to accomplish.
+
+*/
 
 const SHA256 = require('crypto-js/sha256')
-const DIFFICULTY = 4 // Number of initial zeros required in the hash
+const DIFFICULTY = 6 // Number of initial zeros required in the hash
 const MINING_REWARD = 5
 
 class Transaction {
@@ -30,23 +61,26 @@ class Block {
         this.previousHash = previousHash
         this.hash = this.calculateHash()
         this.nonce = 0
+        this.workTime = 0
     }
 
     calculateHash() {
-        return SHA256(this.index +
-            this.previousHash +
-            this.timestamp +
+        const hash = SHA256(this.timestamp +
             this.nonce +
+            this.previousHash +
             JSON.stringify(this.transactions)).toString()
+        return hash
     }
 
     mineBlock({difficulty}) {
+        const startTime = Date.now()
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
             this.nonce++;
             this.hash = this.calculateHash()
         }
+        const endTime = Date.now()
+        return endTime - startTime
     }
-
 }
 
 class Blockchain {
@@ -59,7 +93,7 @@ class Blockchain {
 
     createGenesisBlock() {
         const genesisBlock = new Block({transactions: []})
-        genesisBlock.mineBlock({difficulty: this.difficulty})
+        genesisBlock.workTime = genesisBlock.mineBlock({difficulty: this.difficulty})
         return genesisBlock
     }
 
@@ -68,8 +102,8 @@ class Blockchain {
     }
 
     minePendingTransactions({miningRewardAddress}) {
-        const block = new Block({transactions: this.pendingTransactions,previousHash:this.getLatestBlock().hash})
-        block.mineBlock({difficulty: this.difficulty})
+        const block = new Block({transactions: this.pendingTransactions, previousHash: this.getLatestBlock().hash})
+        block.workTime = block.mineBlock({difficulty: this.difficulty})
 
         this.chain.push(block)
 
@@ -116,6 +150,14 @@ class Blockchain {
         return true
     }
 
+    getAverageWorkTime() {
+        let sum = 0
+        this.chain.forEach(block => {
+            sum += block.workTime
+        })
+        return sum / this.chain.length
+    }
+
     formatDate(date) {
 
         let hours = date.getHours()
@@ -130,7 +172,7 @@ class Blockchain {
         let ret = ''
         for (const block of this.chain) {
             const date = new Date(block.timestamp);
-            ret += `${this.formatDate(date)} - ${block.hash.substring(0,6)}|${block.previousHash.substring(0,6)} - `
+            ret += `${this.formatDate(date)} - ${block.hash.substring(0, 6)}|${block.previousHash.substring(0, 6)} - `
             const tranStrs = []
             for (const transaction of block.transactions) {
                 tranStrs.push(transaction.toString())
@@ -147,11 +189,11 @@ const georgeCoin = new Blockchain()
 georgeCoin.minePendingTransactions({miningRewardAddress: 'geocolumbus@gmail.com'})
 
 // create coins
-for (let i=0; i<10; i++) {
+for (let i = 0; i < 2; i++) {
     georgeCoin.minePendingTransactions({miningRewardAddress: 'george'})
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 2; i++) {
     const transaction = new Transaction({
         fromAddress: addresses[Math.floor(Math.random() * addresses.length)],
         toAddress: addresses[Math.floor(Math.random() * addresses.length)],
@@ -172,4 +214,5 @@ addresses.forEach(address => {
     console.log(`${address}: ${georgeCoin.getBalanceOfAddress(address)}`)
 })
 
-console.log(`\nisChainValid = ${georgeCoin.isChainValid()}`)
+console.log(`\nChain validity check = ${georgeCoin.isChainValid()}`)
+console.log(`Average work time = ${georgeCoin.getAverageWorkTime()} msec`)
